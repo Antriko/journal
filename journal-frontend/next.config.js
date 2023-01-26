a30b9be1fb71f10668673c4a6311f109e62a0d5c
Antriko <creeperdrag772@gmail.com>
2023-01-26 15:51:57 +0000

    prod

--- journal-frontend/next.config.js
+++ journal-frontend/next.config.js
@@ -11,5 +11,12 @@ module.exports = {
       }
       return config
     },
-    nextConfig
+    nextConfig,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
   }
